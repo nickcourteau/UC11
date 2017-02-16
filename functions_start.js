1acb66c435d936feb44beb7a15f27a889a3798e1
Matthew R Marino <mm02971@srcs.loc>
2017-02-16 09:20:34 -0800

    fORMAT

--- functions_start.js
+++ functions_start.js
@@ -1,37 +1,43 @@
-//THESE FUNCTIONS CONTAIN BUGS!!!!!
+//THESE FUNCTIONS CONTAIN no BUGS!!!!!
 
 /**
  * Returns a array of odd numbers from an array
  * @param {Array} a An array of numbers
  * @returns {Array} An array of odd numbers
  */
-function oddFilter(a) {
+function oddFilter(a)
+    {
     var result = [];
-    for (i = 0; i < a.length; ++i) {
-        if (a[i] % 2 != 0) {
+    for (i = 0; i < a.length; ++i)
+        {
+        if (a[i] % 2 != 0)
+            {
             result.push(a[i]);
+            }
         }
-    }
     return result;
-}
+    }
 
 /**
  * Returns an array of the first n triangular numbers
  * @param {number} n The number of triangular numbers to be returned
  * @returns {Array} A list of the triangular numbers
  */
-function listTriangularNumbers(n) {
+function listTriangularNumbers(n)
+    {
     var num;
     var array = [];
-    for (i = 1; i <= n; ++i) {
+    for (i = 1; i <= n; ++i)
+        {
         num = i;
-        for (j = i; j > 1; --j) {
+        for (j = i; j > 1; --j)
+            {
             num = num + j -1;
-        }
+            }
         array.push(num);
-    }
+        }
     return array;
-}
+    }
 
 /**
  * Creates an HTML times table
@@ -39,33 +45,38 @@ function listTriangularNumbers(n) {
  * @param {number} col The column number
  * @returns {string} The HTML table
  */
-function multTable(row, col) {
+function multTable(row, col)
+    {
     var table = "<table>";
     var x;
-    for (var i = 1; i < col; i++) {
+    for (var i = 1; i < col; i++)
+        {
         table = table + "<tr>";
-        for (var j = 1; j < row + 2; j++) {
+        for (var j = 1; j < row + 2; j++)
+            {
             x = i * j;
             table = table + "<td>" + x + "</td>";
-        }
+            }
         table = table + "</tr>";
-    }
+        }
     table = table + "</table>";
     return table;
-}
+    }
 
 /**
  * Returns n!
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
+function factorial(n)
+    {
     var result = 1;
-    for (var i = n; i > 1; i--) {
+    for (var i = n; i > 1; i--)
+        {
         result = result * i;
-    }
+        }
     return result;
-}
+    }
 
 /**
  * Returns combinations nCk
@@ -73,27 +84,31 @@ function factorial(n) {
  * @param {number} k The number of items to be selected
  * @returns {number} nCk
  */
-function combinations(n, k) {
+function combinations(n, k)
+    {
     var c;
     c = factorial(n) / (factorial(k) * (factorial(n - k)));
     return c;
-}
+    }
 
 /**
  * Arranges an array from least to greatest
  * @param {Array} a An array to sort
  * @returns {Array} The sorted array
  */
-function sort(a) {
+function sort(a)
+    {
     result = a;
-    for (var i = 0; i < result.length; ++i) {
+    for (var i = 0; i < result.length; ++i)
+        {
         var j = result[i];
         var k;
         for (k = i - 1;
-            (k >= 0) && (j < result[k]); k--) {
+        (k >= 0) && (j < result[k]); k--)
+            {
             result[k + 1] = result[k];
-        }
+            }
         result[k + 1] = j;
-    }
+        }
     return result;
-}
+    }
